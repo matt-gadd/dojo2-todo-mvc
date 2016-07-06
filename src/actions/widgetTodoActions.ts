40a0939cc0c21beb4f72a4ddc16acd71be5abab8
Matthew Gadd <mgadd@sitepen.com>
2016-07-06 13:12:09 +0100

    remove if

--- src/actions/widgetTodoActions.ts
+++ src/actions/widgetTodoActions.ts
@@ -35,12 +35,10 @@ export const deleteTodo: AnyAction = createAction({
 	do(options: any) {
 		const { widgetStore } = <any> this;
 		const { deletes, afterAll } = options;
-		if (deletes.length) {
-			const item = deletes[0];
-			const children = afterAll.filter((todoItem: any) => todoItem.id !== item)
-			.map((item: any) => item.id);
-			return widgetStore.delete(item).patch({ id: 'todo-list', children });
-		}
+		const item = deletes[0];
+		const children = afterAll.filter((todoItem: any) => todoItem.id !== item)
+		.map((item: any) => item.id);
+		return widgetStore.delete(item).patch({ id: 'todo-list', children });
 	}
 });
 
@@ -49,24 +47,22 @@ export const putTodo: AnyAction = createAction({
 	do(options: any) {
 		const { widgetStore } = <any> this;
 		const { puts, beforeAll } = options;
-		if (puts.length) {
-			const item = puts[0];
-			const children = beforeAll.map((child: any) => child.id);
+		const item = puts[0];
+		const children = beforeAll.map((child: any) => child.id);
 
-			function put() {
-				return widgetStore
-				.put(item)
-				.patch({id: 'todo-list', children: [...children, item.id]});
-			}
-
-			function patch() {
-				return widgetStore
-				.patch(item)
-				.patch({id: 'todo-list', children});
-			}
-
-			return children.includes(item.id) ? patch() : put();
+		function put() {
+			return widgetStore
+			.put(item)
+			.patch({id: 'todo-list', children: [...children, item.id]});
 		}
+
+		function patch() {
+			return widgetStore
+			.patch(item)
+			.patch({id: 'todo-list', children});
+		}
+
+		return children.includes(item.id) ? patch() : put();
 	}
 });
 
